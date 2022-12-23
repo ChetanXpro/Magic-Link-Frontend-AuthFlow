de4dab6626d2540cd388c84e1d577749b634770f
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-12-23 12:39:53 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className="flex bg-[#05386b] text-white items-center gap-4 p-4 justify-between h-12 w-[100vw]">
       <Link to={"/"}>Home</Link>
-      <Link to={"/setting"}>Setting</Link>
+     
 
       <div>
         <Avatar name={`${user?.email}`} size={"sm"} />
