3979c848250154a2e2faae21f222cc9ae11b3c81
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-12-23 12:31:00 +0530

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -3,7 +3,7 @@ import axios, { AxiosError } from "axios";
 // import useAuth from "./useAuth";
 // import useRefreshToken from "./useRefreshToken";
 
-const API_BASE_URL = "http://localhost:5000/";
+const API_BASE_URL = "https://fair-plum-barnacle-tie.cyclic.app/";
 export const apiInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
